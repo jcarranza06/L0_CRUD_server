082c738f997636cb85918b7ab84778773b7508e6
luisda <lusanchezmo@unal.edu.co>
2024-04-15 15:02:47 -0500

    modificacion del puerto env.PORT

--- src/db/dbConnection.js
+++ src/db/dbConnection.js
@@ -1,11 +1,13 @@
-const mysql = require('mysql')
+
 require('dotenv').config({ path: './../.env' })
+const mysql = require('mysql')
 
 const dbConnection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER_NAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE_NAME
+    database: process.env.DB_DATABASE_NAME,
+    port: process.env.DB_PORT
 })
 
 /*dbConnection.connect((err) => {
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ require('dotenv').config({ path: './../.env' })
 const express = require('express')
 
 const app = express()
-const port = process.env.DB_PORT
+const port = process.env.PORT || 3000
 
 const routes = require('./api/endPoints')
 const cors = require('cors');
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-    origin: ["https://l0-crud-client.vercel.app"],
+    origin: ["http://localhost:5173"],
     methods: ["GET", "POST"]
 }));
 
