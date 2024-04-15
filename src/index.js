229a15cf67c36e3b52b41a104f9de00d3e98bad1
luisda <lusanchezmo@unal.edu.co>
2024-04-15 15:40:36 -0500

    modificacion del cors

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://l0-crud-client.onrender.com"],
     methods: ["GET", "POST"]
 }));
 
